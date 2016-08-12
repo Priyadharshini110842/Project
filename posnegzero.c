3ae6d3679b33da510493ca98a2351ebf73d453d0
Priyadharshini110842 <priyanarayanan8296@gmail.com>
2016-08-12 15:56:22 +0530

    Add files via upload

---
+++ posnegzero.c
@@ -0,0 +1,20 @@
+#include,stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a>0)
+{
+printf("%d This is positive number',a);
+if(a==0)
+{
+printf("%d This is zero",a);
+}
+else
+{
+printf("%d This is negative number",a);
+}
+}
+getch();
+}
