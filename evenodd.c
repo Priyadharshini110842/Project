75c979ba2c7fadc5d1530f06b4496b7c2560dc71
Priyadharshini110842 <priyanarayanan8296@gmail.com>
2016-08-12 15:46:52 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("%d given no is even",a);
+}
+else
+{
+printf("%d given no is odd",a);
+}
+getch();
+}
