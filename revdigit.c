0ce1b99b300406c1fc7b04ed4facaab42db46efa
Priyadharshini110842 <priyanarayanan8296@gmail.com>
2016-08-13 17:08:07 +0530

    Update revdigit.c

--- revdigit.c
+++ revdigit.c
@@ -7,9 +7,9 @@ scanf("%d",&n);
 printf("enter the no=");
 while(n!=0)
 {
-reverse=reverse*10;
-reverse=reverse*(n%10);
+reverse=reverse * 10;
+reverse=reverse + n%10;
 n=n/10;
 }
-printf("reverse no",reverse);
+printf("reverse no %d",reverse);
 }
