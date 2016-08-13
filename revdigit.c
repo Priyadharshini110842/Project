6da93f7fc38c5f1eef254c1cc7bae94d4d9693b4
Priyadharshini110842 <priyanarayanan8296@gmail.com>
2016-08-13 17:06:46 +0530

    Add files via upload

---
+++ revdigit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,reverse=0;
+scanf("%d",&n);
+printf("enter the no=");
+while(n!=0)
+{
+reverse=reverse*10;
+reverse=reverse*(n%10);
+n=n/10;
+}
+printf("reverse no",reverse);
+}
