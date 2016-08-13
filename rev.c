160f33fcfbb50ca7b7f458f0bf894533e5673930
Priyadharshini110842 <priyanarayanan8296@gmail.com>
2016-08-13 15:44:42 +0530

    Add files via upload

---
+++ rev.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+char a[5];
+char *r;
+printf("string:");
+scanf("%s",&a);
+r=strrev(a);
+printf("reverse string:");
+return 0;
+getch();
+}
